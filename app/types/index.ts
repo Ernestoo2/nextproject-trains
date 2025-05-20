c93c6d0b39dbf357fb07481ceaa63751825ef3d4
Eze Ernest Chibueze <ezechibuezeernest@gmail.com>
2025-05-21 00:04:00 +0100

    heew

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,14 +1,9 @@
 // Base Types
 export * from './shared/base.types';
-
-// API Types
-export * from './shared/api.types';
-
+ 
 // Component Types
 export * from './components/train.types';
 
 // Feature Types
 export * from './booking.types';
-
-// Validation Types
-export * from './shared/train.types'; 
+ 
