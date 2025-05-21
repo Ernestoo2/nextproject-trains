bee50822e1d7361f02b50db94453ec15e67527d2
Eze Ernest Chibueze <ezechibuezeernest@gmail.com>
2025-05-22 00:51:03 +0100

    check

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -4,11 +4,12 @@ import { getServerSession } from 'next-auth';
 import PageRoute from "./_components/page-route/page";
 import BackImageUi from "./_components/back-image/page";
 import Image from "next/image";
-import image from "../../../public/Assets/Train.png";
+import image from "../../public/Assets/Train.png";
 import NewsletterPage from "./_components/newsletter/page";
 import Review from "./_components/review-page/page";
 import BookingPage from "./_components/booking-interface/page";
 export const dynamic = 'force-dynamic';
+
 
 export default async function DashboardContent() {
   const session = await getServerSession();
